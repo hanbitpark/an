e522ad58382de46234a9fd69312782b0a827873a
hanbitpark <romaniankai@gmail.com>
2016-12-07 16:24:07 +0900

    start

--- app/src/main/java/com/example/admin/myapplication/Day07_inflater.java
+++ app/src/main/java/com/example/admin/myapplication/Day07_inflater.java
@@ -1,7 +1,13 @@
 package com.example.admin.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Day07_inflater extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class Day07_inflater extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day07_inflater);
+    }
+
+    public void onButton1Cliecked(View v){
+         LinearLayout container = (LinearLayout)findViewById(R.id.container);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.activity_day07_inflater_sub, container, true);
+
+        //sub xml 작동 소스
+        container.findViewById(R.id.button);
     }
 }
