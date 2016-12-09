59141c88077be5e7247ccdbf795213c210f25b62
hanbitpark <romaniankai@gmail.com>
2016-12-09 15:02:50 +0900

    start

---
+++ app/src/main/java/com/example/admin/myapplication/Day09_service.java
@@ -0,0 +1,13 @@
+package com.example.admin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Day09_service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day09_service);
+    }
+}
